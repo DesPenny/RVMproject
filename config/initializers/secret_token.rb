26114767348ddd4b2f80e7663c1b7713fef95018
root <root@localhost.localdomain>
2013-04-25 22:12:35 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RVMproject::Application.config.secret_token = 'cc9ef72298135be24a45da0da44648418be7ae88416a4e719406fc64ed9d1d45928d17562b6432181c3bbf05b2a1f4615edde1e6a85818e974560fef3b2bc0ff'
